3fcacef83e04a7ca210eb559140835cc0579042b
wangpeng <657511017@qq.com>
2021-07-06 23:22:38 +0800

    review chapter5 section 5.8 concatenation

---
+++ chapter5/example_frame/concatenation.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+
+string::size_type width(const vector<string>& v)
+{
+    string::size_type maxlen = 0;
+    for (vector<string>::size_type i = 0; i != v.size(); ++i)
+    {
+        maxlen = std::max(v[i].size(), maxlen);
+    }
+    return maxlen;
+}
+
+vector<string> vcat(const vector<string>& top, const vector<string>& bottom)
+{
+    // copy the top picture
+    vector<string> ret = top;
+    
+    // copy entire bottom picture
+    // for (vector<string>::const_iterator it = bottom.begin();
+    //     it != bottom.end(); ++it)
+    // {
+    //     ret.push_back(*it);
+    // }
+
+    // we can use insert instead for loop
+    ret.insert(ret.end(), bottom.begin(),bottom.end());
+    return ret;
+}
+
+vector<string> hcat(const vector<string>& left, const vector<string>& right)
+{
+    vector<string> ret;
+
+    // add 1 to leave a space between pictures
+    string::size_type width1 = width(left) + 1;
+
+    // indices to look at elements from left and right respectively
+    vector<string>::size_type i = 0, j = 0;
+
+    while (i != left.size() || j != right.size())
+    {
+        string s;
+
+        if(i != left.size())
+        {
+            s = left[i++];
+        }
+        
+        s += string(width1 - s.size(), ' ');
+
+        if (j != right.size())
+        {
+            s = right[j++];
+        }
+
+        ret.push_back(s);
+    }
+    return ret;
+    
+}
+
--- chapter5/example_frame/frame.cpp
+++ chapter5/example_frame/frame.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <cctype>
 using std::vector;
 using std::string;
+using namespace std;
 
 string::size_type width(const vector<string>& v)
 {
@@ -17,5 +18,30 @@ string::size_type width(const vector<string>& v)
 
 vector<string> frame(const vector<string>& v)
 {
+    vector<string> ret;
+    string::size_type maxlen = width(v);
 
+    // write the top border
+    string border = string(maxlen + 4, '*');
+    ret.push_back(border);
+
+    // write each line
+    for (vector<string>::size_type i = 0; i != v.size(); ++i)
+    {
+        ret.push_back("* " + v[i] + string(maxlen - v[i].size(), ' ') +  " *");
+    }
+
+    // write the bottom border
+    ret.push_back(border);
+    
+    return ret;
+}
+
+int main()
+{
+    vector<string> test = {"bitcoin", "filecoin", "sfil"};
+    vector<string> res = frame(test);
+    for (auto x : res) cout << x << endl;
+
+    return 0;
 }
